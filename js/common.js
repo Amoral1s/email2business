48a462186918d24121895e2a7acf110162e31296
Дмитрий Филиппов <Amoralis.19@gmail.com>
2020-05-24 19:18:12 +0300

    Add files via upload

--- js/common.js
+++ js/common.js
@@ -5,4 +5,15 @@ $(document).ready(function () {
   }, function(){
     jQuery(this).children('ul').slideUp(100);
   });
+
+  jQuery('.mob-menu').on('click', function() { 
+    jQuery(this).addClass('mob-menu-active');
+    jQuery('.header-wrap-menu').toggleClass('header-wrap-menu-active');
+    jQuery('.header-wrap-menu').slideToggle(300);
+  });
+  jQuery('.close-mob').on('click', function() { 
+    jQuery(this).removeClass('mob-menu-active');
+    jQuery('.header-wrap-menu').removeClass('header-wrap-menu-active');
+    jQuery('.header-wrap-menu').slideUp(300);
+  });
 });
